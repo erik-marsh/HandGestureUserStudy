f66b5daae09ccc8aadfd64c528928e686ed64281
Erik Marsh <erik.i.marsh@gmail.com>
2023-04-17 16:55:04 -0700

    Test integration of the LeapSDK

--- window.cpp
+++ window.cpp
@@ -1,13 +1,296 @@
 #include <cmath>
 #include <iostream>
+#include <sstream>
+#include <string>
+#include <array>
 
-#include "raylib/src/raylib.h"
+#include "LeapSDK/include/Leap.h"
+#include "raylib-depolluted.h"
+
+class SampleListener : public Leap::Listener
+{
+   public:
+    virtual void onInit(const Leap::Controller&);
+    virtual void onConnect(const Leap::Controller&);
+    virtual void onDisconnect(const Leap::Controller&);
+    virtual void onExit(const Leap::Controller&);
+    virtual void onFrame(const Leap::Controller&);
+    virtual void onFocusGained(const Leap::Controller&);
+    virtual void onFocusLost(const Leap::Controller&);
+    virtual void onDeviceChange(const Leap::Controller&);
+    virtual void onServiceConnect(const Leap::Controller&);
+    virtual void onServiceDisconnect(const Leap::Controller&);
+
+   private:
+};
+
+const std::array<std::string, 5> fingerNames = {"Thumb", "Index", "Middle", "Ring", "Pinky"};
+const std::array<std::string, 4> boneNames = {"Metacarpal", "Proximal", "Middle", "Distal"};
+const std::array<std::string, 4> stateNames = {"STATE_INVALID", "STATE_START", "STATE_UPDATE", "STATE_END"};
+
+void SampleListener::onInit(const Leap::Controller& controller)
+{
+    std::cout << "[LeapMotion Controller] Initialized" << std::endl;
+}
+
+void SampleListener::onConnect(const Leap::Controller& controller)
+{
+    std::cout << "[LeapMotion Controller] Connected" << std::endl;
+}
+
+void SampleListener::onDisconnect(const Leap::Controller& controller)
+{
+    // Note: not dispatched when running in a debugger.
+    std::cout << "[LeapMotion Controller] Disconnected" << std::endl;
+}
+
+void SampleListener::onExit(const Leap::Controller& controller)
+{
+    std::cout << "[LeapMotion Controller] Exited" << std::endl;
+}
+
+void SampleListener::onFrame(const Leap::Controller& controller)
+{
+    return;
+    // Get the most recent frame and report some basic information
+    const Leap::Frame frame = controller.frame();
+    // std::cout << "Frame id: " << frame.id() << ", timestamp: " << frame.timestamp()
+    //           << ", hands: " << frame.hands().count()
+    //           << ", extended fingers: " << frame.fingers().extended().count()
+    //           << ", tools: " << frame.tools().count() << ", gestures: " <<
+    //           frame.gestures().count()
+    //           << std::endl;
+
+    Leap::HandList hands = frame.hands();
+    for (Leap::HandList::const_iterator hl = hands.begin(); hl != hands.end(); ++hl)
+    {
+        // Get the first hand
+        const Leap::Hand hand = *hl;
+        std::string handType = hand.isLeft() ? "Left hand" : "Right hand";
+        std::cout << std::string(2, ' ') << handType << ", id: " << hand.id()
+                  << ", palm position: " << hand.palmPosition() << std::endl;
+        // Get the hand's normal vector and direction
+        const Leap::Vector normal = hand.palmNormal();
+        const Leap::Vector direction = hand.direction();
+
+        // Calculate the hand's pitch, roll, and yaw angles
+        std::cout << std::string(2, ' ') << "pitch: " << direction.pitch() * Leap::RAD_TO_DEG
+                  << " degrees, "
+                  << "roll: " << normal.roll() * Leap::RAD_TO_DEG << " degrees, "
+                  << "yaw: " << direction.yaw() * Leap::RAD_TO_DEG << " degrees" << std::endl;
+
+        // Get the Arm bone
+        Leap::Arm arm = hand.arm();
+        std::cout << std::string(2, ' ') << "Arm direction: " << arm.direction()
+                  << " wrist position: " << arm.wristPosition()
+                  << " elbow position: " << arm.elbowPosition() << std::endl;
+
+        // Get fingers
+        const Leap::FingerList fingers = hand.fingers();
+        for (Leap::FingerList::const_iterator fl = fingers.begin(); fl != fingers.end(); ++fl)
+        {
+            const Leap::Finger finger = *fl;
+            std::cout << std::string(4, ' ') << fingerNames[finger.type()]
+                      << " finger, id: " << finger.id() << ", length: " << finger.length()
+                      << "mm, width: " << finger.width() << std::endl;
+
+            // Get finger bones
+            for (int b = 0; b < 4; ++b)
+            {
+                Leap::Bone::Type boneType = static_cast<Leap::Bone::Type>(b);
+                Leap::Bone bone = finger.bone(boneType);
+                std::cout << std::string(6, ' ') << boneNames[boneType]
+                          << " bone, start: " << bone.prevJoint() << ", end: " << bone.nextJoint()
+                          << ", direction: " << bone.direction() << std::endl;
+            }
+        }
+    }
+
+    // // Get tools
+    // const Leap::ToolList tools = frame.tools();
+    // for (Leap::ToolList::const_iterator tl = tools.begin(); tl != tools.end(); ++tl)
+    // {
+    //     const Leap::Tool tool = *tl;
+    //     std::cout << std::string(2, ' ') << "Tool, id: " << tool.id()
+    //               << ", position: " << tool.tipPosition() << ", direction: " << tool.direction()
+    //               << std::endl;
+    // }
+
+    // Get gestures
+    // const Leap::GestureList gestures = frame.gestures();
+    // for (int g = 0; g < gestures.count(); ++g)
+    // {
+    //     Leap::Gesture gesture = gestures[g];
+
+    //     switch (gesture.type())
+    //     {
+    //         case Leap::Gesture::TYPE_CIRCLE:
+    //         {
+    //             Leap::CircleGesture circle = gesture;
+    //             std::string clockwiseness;
+
+    //             if (circle.pointable().direction().angleTo(circle.normal()) <= Leap::PI / 2)
+    //             {
+    //                 clockwiseness = "clockwise";
+    //             }
+    //             else
+    //             {
+    //                 clockwiseness = "counterclockwise";
+    //             }
+
+    //             // Calculate angle swept since last frame
+    //             float sweptAngle = 0;
+    //             if (circle.state() != Leap::Gesture::STATE_START)
+    //             {
+    //                 Leap::CircleGesture previousUpdate =
+    //                     Leap::CircleGesture(controller.frame(1).gesture(circle.id()));
+    //                 sweptAngle = (circle.progress() - previousUpdate.progress()) * 2 * Leap::PI;
+    //             }
+    //             std::cout << std::string(2, ' ') << "Circle id: " << gesture.id()
+    //                       << ", state: " << stateNames[gesture.state()]
+    //                       << ", progress: " << circle.progress() << ", radius: " <<
+    //                       circle.radius()
+    //                       << ", angle " << sweptAngle * Leap::RAD_TO_DEG << ", " << clockwiseness
+    //                       << std::endl;
+    //             break;
+    //         }
+    //         case Leap::Gesture::TYPE_SWIPE:
+    //         {
+    //             Leap::SwipeGesture swipe = gesture;
+    //             std::cout << std::string(2, ' ') << "Swipe id: " << gesture.id()
+    //                       << ", state: " << stateNames[gesture.state()]
+    //                       << ", direction: " << swipe.direction() << ", speed: " << swipe.speed()
+    //                       << std::endl;
+    //             break;
+    //         }
+    //         case Leap::Gesture::TYPE_KEY_TAP:
+    //         {
+    //             Leap::KeyTapGesture tap = gesture;
+    //             std::cout << std::string(2, ' ') << "Key Tap id: " << gesture.id()
+    //                       << ", state: " << stateNames[gesture.state()]
+    //                       << ", position: " << tap.position() << ", direction: " <<
+    //                       tap.direction()
+    //                       << std::endl;
+    //             break;
+    //         }
+    //         case Leap::Gesture::TYPE_SCREEN_TAP:
+    //         {
+    //             Leap::ScreenTapGesture screentap = gesture;
+    //             std::cout << std::string(2, ' ') << "Screen Tap id: " << gesture.id()
+    //                       << ", state: " << stateNames[gesture.state()]
+    //                       << ", position: " << screentap.position()
+    //                       << ", direction: " << screentap.direction() << std::endl;
+    //             break;
+    //         }
+    //         default:
+    //             std::cout << std::string(2, ' ') << "Unknown gesture type." << std::endl;
+    //             break;
+    //     }
+    // }
+
+    if (!frame.hands().isEmpty())  // || !gestures.isEmpty())
+    {
+        std::cout << std::endl;
+    }
+}
+
+void SampleListener::onFocusGained(const Leap::Controller& controller)
+{
+    std::cout << "[LeapMotion Controller] Focus Gained" << std::endl;
+}
+
+void SampleListener::onFocusLost(const Leap::Controller& controller)
+{
+    std::cout << "[LeapMotion Controller] Focus Lost" << std::endl;
+}
+
+void SampleListener::onDeviceChange(const Leap::Controller& controller)
+{
+    std::cout << "[LeapMotion Controller] Device Changed" << std::endl;
+    const Leap::DeviceList devices = controller.devices();
+
+    for (int i = 0; i < devices.count(); ++i)
+    {
+        std::cout << "id: " << devices[i].toString() << std::endl;
+        std::cout << "  isStreaming: " << (devices[i].isStreaming() ? "true" : "false")
+                  << std::endl;
+    }
+}
+
+void SampleListener::onServiceConnect(const Leap::Controller& controller)
+{
+    std::cout << "[LeapMotion Controller] Service Connected" << std::endl;
+}
+
+void SampleListener::onServiceDisconnect(const Leap::Controller& controller)
+{
+    std::cout << "[LeapMotion Controller] Service Disconnected" << std::endl;
+}
+
+std::string StringifyFrame(Leap::Frame& frame)
+{
+    std::stringstream ss;
+    ss << "Frame id: " << frame.id() << "\nTimestamp: " << frame.timestamp()
+       << "\nHands: " << frame.hands().count()
+       << "\nExtended fingers: " << frame.fingers().extended().count() << "\n";
+
+    auto hands = frame.hands();
+    for (auto handIter = hands.begin(); handIter != hands.end(); handIter++)
+    {
+        auto hand = *handIter;
+
+        std::string handType = hand.isLeft() ? "Left hand" : "Right hand";
+        const Leap::Vector normal = hand.palmNormal();
+        const Leap::Vector direction = hand.direction();
+
+        ss << handType << " (id: " << hand.id() << ")\n"
+           << "  palm position: " << hand.palmPosition() << "\n"
+           << "  palm normal: " << normal << "\n"
+           << "  hand direction: " << direction << "\n";
+
+        // Calculate the hand's pitch, roll, and yaw angles
+        ss << "  pitch: " << direction.pitch() * Leap::RAD_TO_DEG << " degrees\n"
+           << "  roll: " << normal.roll() * Leap::RAD_TO_DEG << " degrees\n"
+           << "  yaw: " << direction.yaw() * Leap::RAD_TO_DEG << " degrees\n";
+
+        // Get the Arm bone
+        Leap::Arm arm = hand.arm();
+        ss << "  Arm direction: " << arm.direction() << "\n"
+           << "  wrist position: " << arm.wristPosition() << "\n"
+           << "  elbow position: " << arm.elbowPosition() << "\n";
+
+        // Get fingers
+        const Leap::FingerList fingers = hand.fingers();
+        for (Leap::FingerList::const_iterator fl = fingers.begin(); fl != fingers.end(); ++fl)
+        {
+            const Leap::Finger finger = *fl;
+            ss << "  " << fingerNames[finger.type()] << " finger (id: " << finger.id() << ")\n";
+
+            // Get finger bones
+            for (int b = 0; b < 4; ++b)
+            {
+                Leap::Bone::Type boneType = static_cast<Leap::Bone::Type>(b);
+                Leap::Bone bone = finger.bone(boneType);
+                ss << "    " << boneNames[boneType] << " bone\n"
+                   << "      start: " << bone.prevJoint() << "\n"
+                   << "      end: " << bone.nextJoint() << "\n"
+                   << "      direction: " << bone.direction() << "\n";
+            }
+        }
+    }
+
+    return ss.str();
+}
 
 int main()
 {
-    std::cout << "test" << std::endl;
-    constexpr int screenWidth = 800;
-    constexpr int screenHeight = 450;
+    SampleListener listener;
+    Leap::Controller controller;
+
+    controller.addListener(listener);
+
+    constexpr int screenWidth = 1600;
+    constexpr int screenHeight = 900;
 
     InitWindow(screenWidth, screenHeight, "something something idk");
 
@@ -19,33 +302,55 @@ int main()
     camera.fovy = 45.0f;
     camera.projection = CAMERA_PERSPECTIVE;
 
-    Vector3 cubePosition = {0.0f, 0.0f, 0.0f};
-
-    DisableCursor();
+    //DisableCursor();
     SetTargetFPS(60);
 
     while (!WindowShouldClose())
     {
-        UpdateCamera(&camera, CAMERA_THIRD_PERSON);
+        UpdateCamera(&camera, CAMERA_FREE);
 
-        if (IsKeyDown('Z')) camera.target = (Vector3){0.0f, 0.0f, 0.0f};
+        if (IsKeyDown('Z'))
+        {
+            camera.target = (Vector3){0.0f, 0.0f, 0.0f};
+            camera.up = (Vector3){0.0f, 1.0f, 0.0f};
+        }
+        Leap::Frame leapFrame = controller.frame();
+        std::string frameString = StringifyFrame(leapFrame);
 
         BeginDrawing();
         {
-            ClearBackground(RAYWHITE);
+            ClearBackground(LIGHTGRAY);
             BeginMode3D(camera);
             {
                 double currTime = GetTime();
 
-                Vector3 start = {0.0f, 0.0f, 0.0f};
-                Vector3 end = {1.0 * std::sin(currTime), 1.0f * std::cos(currTime), 0.0f};
-                DrawLine3D(start, end, RED);
+                Vector3 origin = {0.0f, 0.0f, 0.0f};
+                Vector3 basisI = {10.0f, 0.0f, 0.0f};
+                Vector3 basisJ = {0.0f, 10.0f, 0.0f};
+                Vector3 basisK = {0.0f, 0.0f, 10.0f};
+
+                DrawLine3D(origin, basisI, RED);
+                DrawLine3D(origin, basisJ, GREEN);
+                DrawLine3D(origin, basisK, BLUE);
+
+                auto hands = leapFrame.hands();
+                for (auto it = hands.begin(); it != hands.end(); it++)
+                {
+                    auto hand = *it;
+                    Color col = hand.isLeft() ? ORANGE : VIOLET;
+                    auto pnorm = hand.palmNormal();
+                    Vector3 pnormRaylib = {pnorm.x, pnorm.y, pnorm.z};
+                    DrawLine3D(origin, pnormRaylib, col);
+                }
             }
             EndMode3D();
+
+            DrawText(frameString.c_str(), 10, 10, 10, BLACK);
         }
         EndDrawing();
     }
 
     CloseWindow();
+    controller.removeListener(listener);
     return 0;
 }
