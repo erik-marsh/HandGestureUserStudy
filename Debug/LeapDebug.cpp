e82caf61a55949ce6279fa348330bd6e6df256de
Erik Marsh <erik.i.marsh@gmail.com>
2023-05-08 17:27:07 -0700

    Artifacts of porting to Windows and the LeapC SDK

--- Debug/LeapDebug.cpp
+++ Debug/LeapDebug.cpp
@@ -1,5 +1,8 @@
 #include "LeapDebug.hpp"
 
+#include <sstream>
+#include <string>
+
 namespace Debug
 {
 const std::array<std::string, 5> EventListener::fingerNames = {"Thumb", "Index", "Middle", "Ring",
@@ -9,113 +12,57 @@ const std::array<std::string, 4> EventListener::boneNames = {"Metacarpal", "Prox
 const std::array<std::string, 4> EventListener::stateNames = {"STATE_INVALID", "STATE_START",
                                                               "STATE_UPDATE", "STATE_END"};
 
-void EventListener::onInit(const Leap::Controller& controller)
-{
-    std::cout << "[LeapMotion Controller] Initialized" << std::endl;
-}
-
-void EventListener::onConnect(const Leap::Controller& controller)
-{
-    std::cout << "[LeapMotion Controller] Connected" << std::endl;
-}
-
-void EventListener::onDisconnect(const Leap::Controller& controller)
-{
-    // Note: not dispatched when running in a debugger.
-    std::cout << "[LeapMotion Controller] Disconnected" << std::endl;
-}
-
-void EventListener::onExit(const Leap::Controller& controller)
-{
-    std::cout << "[LeapMotion Controller] Exited" << std::endl;
-}
-
-void EventListener::onFrame(const Leap::Controller& controller) {}
-
-void EventListener::onFocusGained(const Leap::Controller& controller)
-{
-    std::cout << "[LeapMotion Controller] Focus Gained" << std::endl;
-}
-
-void EventListener::onFocusLost(const Leap::Controller& controller)
-{
-    std::cout << "[LeapMotion Controller] Focus Lost" << std::endl;
-}
-
-void EventListener::onDeviceChange(const Leap::Controller& controller)
-{
-    std::cout << "[LeapMotion Controller] Device Changed" << std::endl;
-    const Leap::DeviceList devices = controller.devices();
-
-    for (int i = 0; i < devices.count(); ++i)
-    {
-        std::cout << "id: " << devices[i].toString() << std::endl;
-        std::cout << "  isStreaming: " << (devices[i].isStreaming() ? "true" : "false")
-                  << std::endl;
-    }
-}
-
-void EventListener::onServiceConnect(const Leap::Controller& controller)
-{
-    std::cout << "[LeapMotion Controller] Service Connected" << std::endl;
-}
-
-void EventListener::onServiceDisconnect(const Leap::Controller& controller)
-{
-    std::cout << "[LeapMotion Controller] Service Disconnected" << std::endl;
-}
-
-std::string StringifyFrame(Leap::Frame& frame)
+std::string StringifyFrame(LEAP_TRACKING_EVENT& frame)
 {
     std::stringstream ss;
-    ss << "Frame id: " << frame.id() << "\nTimestamp: " << frame.timestamp()
-       << "\nHands: " << frame.hands().count()
-       << "\nExtended fingers: " << frame.fingers().extended().count() << "\n";
+    ss << "Frame id: " << frame.tracking_frame_id << "\nTimestamp: " << frame.info.timestamp
+       << "\nHands: " << frame.nHands
+       << /*"\nExtended fingers: " << frame.fingers().extended().count() <<*/ "\n";
 
-    auto hands = frame.hands();
-    for (auto handIter = hands.begin(); handIter != hands.end(); handIter++)
+    auto hands = frame.pHands;
+    for (int i = 0; i < frame.nHands; i++)
     {
-        auto hand = *handIter;
+        auto hand = frame.pHands[i];
 
-        std::string handType = hand.isLeft() ? "Left hand" : "Right hand";
-        const Leap::Vector normal = hand.palmNormal();
-        const Leap::Vector direction = hand.direction();
+        std::string handType = hand.type == eLeapHandType::eLeapHandType_Left ? "Left hand" : "Right hand";
+        const LEAP_VECTOR normal = hand.palm.normal;
+        const LEAP_VECTOR direction = hand.palm.direction;
 
-        ss << handType << " (id: " << hand.id() << ")\n"
-           << "  palm position: " << hand.palmPosition() << "\n"
-           << "  palm normal: " << normal << "\n"
-           << "  hand direction: " << direction << "\n";
+        ss << handType << " (id: " << hand.id << ")\n";
+         //   << "  palm position: " << hand.palm.position << "\n"
+         //   << "  palm normal: " << normal << "\n"
+         //   << "  hand direction: " << direction << "\n";
 
         // Calculate the hand's pitch, roll, and yaw angles
-        ss << "  pitch: " << direction.pitch() * Leap::RAD_TO_DEG << " degrees\n"
-           << "  roll: " << normal.roll() * Leap::RAD_TO_DEG << " degrees\n"
-           << "  yaw: " << direction.yaw() * Leap::RAD_TO_DEG << " degrees\n";
+      //   ss << "  pitch: " << direction.pitch() * Leap::RAD_TO_DEG << " degrees\n"
+      //      << "  roll: " << normal.roll() * Leap::RAD_TO_DEG << " degrees\n"
+      //      << "  yaw: " << direction.yaw() * Leap::RAD_TO_DEG << " degrees\n";
 
         // Get the Arm bone
-        Leap::Arm arm = hand.arm();
-        ss << "  Arm direction: " << arm.direction() << "\n"
-           << "  wrist position: " << arm.wristPosition() << "\n"
-           << "  elbow position: " << arm.elbowPosition() << "\n";
+      //   Leap::Arm arm = hand.arm();
+      //   ss << "  Arm direction: " << arm.direction() << "\n"
+      //      << "  wrist position: " << arm.wristPosition() << "\n"
+      //      << "  elbow position: " << arm.elbowPosition() << "\n";
 
         // Get fingers
-        const Leap::FingerList fingers = hand.fingers();
-        for (Leap::FingerList::const_iterator fl = fingers.begin(); fl != fingers.end(); ++fl)
-        {
-            const Leap::Finger finger = *fl;
-            ss << "  " << EventListener::fingerNames[finger.type()]
-               << " finger (id: " << finger.id() << ")\n";
+      //   const Leap::FingerList fingers = hand.fingers();
+      //   for (Leap::FingerList::const_iterator fl = fingers.begin(); fl != fingers.end(); ++fl)
+      //   {
+      //       const Leap::Finger finger = *fl;
+      //       ss << "  " << EventListener::fingerNames[finger.type()]
+      //          << " finger (id: " << finger.id() << ")\n";
 
-            // Get finger bones
-            for (int b = 0; b < 4; ++b)
-            {
-                Leap::Bone::Type boneType = static_cast<Leap::Bone::Type>(b);
-                Leap::Bone bone = finger.bone(boneType);
-                ss << "    " << EventListener::boneNames[boneType] << " bone\n"
-                   << "      start: " << bone.prevJoint() << "\n"
-                   << "      end: " << bone.nextJoint() << "\n"
-                   << "      direction: " << bone.direction() << "\n";
-            }
-        }
+      //       // Get finger bones
+      //       for (int b = 0; b < 4; ++b)
+      //       {
+      //           Leap::Bone::Type boneType = static_cast<Leap::Bone::Type>(b);
+      //           Leap::Bone bone = finger.bone(boneType);
+      //           ss << "    " << EventListener::boneNames[boneType] << " bone\n"
+      //              << "      start: " << bone.prevJoint() << "\n"
+      //              << "      end: " << bone.nextJoint() << "\n"
+      //              << "      direction: " << bone.direction() << "\n";
+      //       }
+      //   }
     }
 
     return ss.str();
--- Debug/LeapDebug.hpp
+++ Debug/LeapDebug.hpp
@@ -1,27 +1,32 @@
-#include "../LeapSDK/include/Leap.h"
+#pragma once
+
+#include <array>
+#include <string>
+
+#include "../LeapSDK/include/LeapC.h"
 
 namespace Debug
 {
 
-class EventListener : public Leap::Listener
+class EventListener //: public Leap::Listener
 {
    public:
-    virtual void onInit(const Leap::Controller&);
-    virtual void onConnect(const Leap::Controller&);
-    virtual void onDisconnect(const Leap::Controller&);
-    virtual void onExit(const Leap::Controller&);
-    virtual void onFrame(const Leap::Controller&);
-    virtual void onFocusGained(const Leap::Controller&);
-    virtual void onFocusLost(const Leap::Controller&);
-    virtual void onDeviceChange(const Leap::Controller&);
-    virtual void onServiceConnect(const Leap::Controller&);
-    virtual void onServiceDisconnect(const Leap::Controller&);
+    // virtual void onInit(const Leap::Controller&);
+    // virtual void onConnect(const Leap::Controller&);
+    // virtual void onDisconnect(const Leap::Controller&);
+    // virtual void onExit(const Leap::Controller&);
+    // virtual void onFrame(const Leap::Controller&);
+    // virtual void onFocusGained(const Leap::Controller&);
+    // virtual void onFocusLost(const Leap::Controller&);
+    // virtual void onDeviceChange(const Leap::Controller&);
+    // virtual void onServiceConnect(const Leap::Controller&);
+    // virtual void onServiceDisconnect(const Leap::Controller&);
 
     const static std::array<std::string, 5> fingerNames;
     const static std::array<std::string, 4> boneNames;
     const static std::array<std::string, 4> stateNames;
 };
 
-std::string StringifyFrame(Leap::Frame& frame);
+std::string StringifyFrame(LEAP_TRACKING_EVENT& frame);
 
 }
