9b1e3a5322f983215a53277027a1cd93cb9d3bed
Erik Marsh <erik.i.marsh@gmail.com>
2023-05-24 13:18:17 -0700

    Some simple verification that RapidJSON works

--- Tests/CppHttplibTest.cpp
+++ Tests/CppHttplibTest.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
 #include "../cpp-httplib/httplib.h"
+#include "../rapidjson/include/rapidjson/rapidjson.h"
+#include "../rapidjson/include/rapidjson/document.h"
 
 int main()
 {
@@ -11,6 +13,12 @@ int main()
 
     server.Post("/test", [](const httplib::Request& req, httplib::Response& res) {
         std::cout << "Got POST request:\n" << req.body << std::endl;
+        rapidjson::Document d;
+        d.Parse(req.body.c_str());
+
+        // interestingly, we get an assertion fail if this parse fails
+        // i'll take it, but i'm used to seeing exceptions in these kinds of cases
+        std::cout << d["value"].GetInt() << std::endl;
     });
 
     server.listen("localhost", 5000);
