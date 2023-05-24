3fe9143503b4dbc0c88aaba37872056ca9475853
Erik Marsh <erik.i.marsh@gmail.com>
2023-05-24 13:26:01 -0700

    Just a few more notes for myself

--- Tests/CppHttplibTest.cpp
+++ Tests/CppHttplibTest.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 
 #include "../cpp-httplib/httplib.h"
-#include "../rapidjson/include/rapidjson/rapidjson.h"
 #include "../rapidjson/include/rapidjson/document.h"
+#include "../rapidjson/include/rapidjson/rapidjson.h"
 
 int main()
 {
@@ -11,16 +11,24 @@ int main()
     server.Get("/test", [](const httplib::Request& req, httplib::Response& res)
                { res.set_content("Hello, world!", "text/plain"); });
 
-    server.Post("/test", [](const httplib::Request& req, httplib::Response& res) {
-        std::cout << "Got POST request:\n" << req.body << std::endl;
-        rapidjson::Document d;
-        d.Parse(req.body.c_str());
+    server.Post("/test",
+                [](const httplib::Request& req, httplib::Response& res)
+                {
+                    std::cout << "Got POST request:\n" << req.body << std::endl;
+                    rapidjson::Document d;
+                    d.Parse(req.body.c_str());
 
-        // interestingly, we get an assertion fail if this parse fails
-        // i'll take it, but i'm used to seeing exceptions in these kinds of cases
-        std::cout << d["value"].GetInt() << std::endl;
-    });
+                    // interestingly, we get an assertion fail if this parse fails
+                    // i'll take it, but i'm used to seeing exceptions in these kinds of cases
+                    std::cout << d["value"].GetInt() << std::endl;
+                });
 
+    server.Get("/stop",
+               [&server](const httplib::Request& req, httplib::Response& res) { server.stop(); });
+
+    std::cout << "Launching server" << std::endl;
+    // this blocks, and server.stop() needs to be called to properly stop the server
     server.listen("localhost", 5000);
+    std::cout << "Exited gracefully" << std::endl;
     return 0;
 }
