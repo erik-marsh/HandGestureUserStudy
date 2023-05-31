82b316fcdbed4c3f7a7b80a5f11d23ed9605b4fb
Erik Marsh <erik.i.marsh@gmail.com>
2023-05-31 11:51:02 -0700

    Update this note to be factually correct

--- Programs/ThreadingTest.cpp
+++ Programs/ThreadingTest.cpp
@@ -5,12 +5,10 @@
 #include "LeapDriver.hpp"
 #include "Visualizer.hpp"
 
-// TODO: There are some flaws with this entire approach:
-// 1) The leap connection crashes every once in a while when doing multithreading.
-// 2) If we update the hand state at 60Hz or less,
-//    there is literally no reason to separate the processing and rendering threads.
-//    We could probably tick the processing thread once every 1ms,
-//    since the processing time seems to be roughly 200-700us every frame.
+// Note: if we update the hand state at 60Hz or less,
+// there is literally no reason to separate the processing and rendering threads.
+// We could probably tick the processing thread once every 1ms,
+// since the processing time seems to be roughly 200-700us every frame.
 int main()
 {
     Input::Leap::LeapConnection connection;
