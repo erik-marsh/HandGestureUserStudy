1ec9c4a79bbb4edf0e64c83a38bdd7c31ee4c66d
Erik Marsh <erik.i.marsh@gmail.com>
2023-06-07 15:15:54 -0700

    Class names now conform to a lowercase + hyphen format

--- www/keystrokes.js
+++ www/keystrokes.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const textFields = document.getElementsByClassName("textInputField");
+const textFields = document.getElementsByClassName("text-input-field");
 Array.from(textFields).forEach(field => {
     const fieldIndex = field.getAttribute("data-field-index");
     const inputTextarea = field.getElementsByClassName("input")[0];
