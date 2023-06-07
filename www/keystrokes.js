72911f8811c1257552f5d41ffa8ada63892ff9c5
Erik Marsh <erik.i.marsh@gmail.com>
2023-06-07 14:22:46 -0700

    Text input validation that sends keystroke data back to the server

--- www/keystrokes.js
+++ www/keystrokes.js
@@ -1,17 +1,56 @@
 "use strict";
 
-const textFields = document.getElementsByClassName('textInputField');
+const textFields = document.getElementsByClassName("textInputField");
 Array.from(textFields).forEach(field => {
-    console.log(field);
     const fieldIndex = field.getAttribute("data-field-index");
     const inputTextarea = field.getElementsByClassName("input")[0];
     const expectedTextarea = field.getElementsByClassName("expected")[0];
-    console.log(inputTextarea);
-    console.log(expectedTextarea);
-    field.addEventListener('keyup', e => {
-        // the way the text field is set up in HTML may yield
-        // leading and trailing whitespace, so we trim those out.
-        const equality = inputTextarea.value === expectedTextarea.value.trim();
-        console.log("Does string match? " + equality);
+    // the way the text field is set up in HTML may yield
+    // leading and trailing whitespace, so we trim those out.
+    const expectedString = expectedTextarea.value.trim();
+
+    const makeKeystrokeEvent = (timestamp, keyupEvent, wasCorrect) => ({
+        "timestamp": timestamp,
+        "wasCorrect": wasCorrect,
+        "key": keyupEvent.key
     });
+
+    let gatheredInputs = [];
+    let completion = {
+        "timestamp": null,
+        "fieldIndex": fieldIndex
+    };
+
+    const listener = e => {
+        const timestamp = Date.now();
+        const equalSoFar = expectedString.startsWith(inputTextarea.value);
+        const equality = inputTextarea.value === expectedString;
+
+        gatheredInputs.push(makeKeystrokeEvent(timestamp, e, equalSoFar));
+
+        inputTextarea.style.color = equalSoFar ? "black" : "red";
+
+        if (equality) {
+            completion.timestamp = timestamp;
+            console.log("Field has been completed, moving on...");
+            inputTextarea.setAttribute("disabled", "");
+            inputTextarea.style.color = "green";
+            field.removeEventListener("keyup", listener);
+            // TODO: need to re-enable background
+
+            const dataToSend = {
+                "keystrokes": gatheredInputs,
+                "completion": completion
+            };
+
+            fetch("/events", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(dataToSend)
+            })
+        }
+    };
+    field.addEventListener("keyup", listener);
 });
