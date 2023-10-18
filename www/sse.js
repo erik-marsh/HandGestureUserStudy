f479a9f5dbbb5dc4d6da8d083e5ad945d0450f9e
Erik Marsh <erik.i.marsh@gmail.com>
2023-10-17 18:17:08 -0700

    Catch keep-alive messages separately; less verbose

--- www/sse.js
+++ www/sse.js
@@ -5,17 +5,12 @@ const eventSource = new EventSource(eventSourceEndpoint);
 
 eventSource.addEventListener("open", e => {
     console.log("[SSE] Connection opened.");
-    console.log(e);
 });
 
 eventSource.addEventListener("error", e => {
     console.log("[SSE] Connection errored.");
-    console.log(e);
 });
 
-// TODO: if we keep a queue of messages around on the server side, 
-// we can keep sending events until the client receives them,
-// preventing the issue where 
 // captures events of type message AND events WITHOUT a type
 // but nothing else
 eventSource.addEventListener("message", e => {
@@ -23,12 +18,11 @@ eventSource.addEventListener("message", e => {
     console.log(e);
 });
 
-eventSource.addEventListener("qux", e => {
-    console.log(e);
-})
+eventSource.addEventListener("keep-alive", e => {
+    console.log(`[SSE] [EventID=${e.lastEventId}] Received keep-alive message.`);
+});
 
 eventSource.addEventListener("proceed", e => {
-    console.log("[SSE] Received proceed message.");
-    console.log(e);
+    console.log(`[SSE] [EventID=${e.lastEventId}] Received proceed message.`);
     setTimeout(() => { location.reload(); }, 500);
 });
