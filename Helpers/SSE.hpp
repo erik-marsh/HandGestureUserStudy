02df3225a64f7e6a0918a7fbb70f32103d63078a
Erik Marsh <erik.i.marsh@gmail.com>
2023-10-17 18:17:46 -0700

    Fixed issue where SSEs get lost seemingly randomly

--- Helpers/SSE.hpp
+++ Helpers/SSE.hpp
@@ -1,9 +1,11 @@
 #pragma once
 
 #include <httplib.h>
+
 #include <Helpers/JSONEvents.hpp>
 #include <atomic>
 #include <condition_variable>
+#include <iostream>
 #include <mutex>
 #include <queue>
 #include <sstream>
@@ -21,15 +23,35 @@ class EventDispatcher
     {
         std::unique_lock<std::mutex> lock(queueMutex);
         cv.wait(lock, [this] { return queueSize > 0 || programTerminated; });
-        // TODO: there's still probably a race condition i didn't account for
         // lock is reacquired...
         if (programTerminated)
             return;
 
         std::string message = messageQueue.front();
-        sink.write(message.c_str(), message.length());
-        messageQueue.pop();
+
+        // if we don't succeed in writing the message, we don't pop it from the queue
+        // the next time this function is called, we attempt to write it again.
+        // when does WaitEvent get called?
+        //     whenever httplib asks the chunked content provider to write something
+        // note that the connection is kept alive for some period of time
+        //     CPPHTTPLIB defines the keep alive timeout as 5 seconds by default
+        //     this value is settable
+        //     (we the server push a message to it alive every 3 seconds)
+        // while this connection is alive,
+        // process_server_socket_core loops until the connection is supposed to close
+        //     (it loops at 1000Hz)
+        // long story short, at some point during process_server_socket_core,
+        // the client is written to again
+        bool succeeded = sink.write(message.c_str(), message.length());
+        if (succeeded)
+            messageQueue.pop();
         queueSize = messageQueue.size();
+
+        // std::stringstream ss;
+        // ss << "[SSE] [queue size=" << queueSize << ", write success=" << std::boolalpha << succeeded
+        //    << "] Sent event: " << message;
+        // std::cout << ss.str();
+        
         // lock is released
     }
 
@@ -72,7 +94,7 @@ void HeartbeatLoop(std::atomic<bool>& isRunning, Helpers::EventDispatcher& dispa
     std::cout << "[main] Starting SSE heartbeat thread...\n";
     while (isRunning)
     {
-        dispatcher.SendEvent("data: keep alive\r\n\r\n");
+        dispatcher.SendEvent("event: keep-alive\r\ndata: keep-alive\r\n\r\n");
         std::unique_lock<std::mutex> lock(heartbeatMutex);
         heartbeatCV.wait_for(lock, std::chrono::seconds(intervalSeconds),
                              [] { return killHeartbeat.load(); });
@@ -80,6 +102,6 @@ void HeartbeatLoop(std::atomic<bool>& isRunning, Helpers::EventDispatcher& dispa
     std::cout << "[main] Stopping SSE heartbeat thread...\n";
 }
 
-}
+}  // namespace
 
 }  // namespace Helpers
