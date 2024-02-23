a3117d726797533a698fef4be2f027caf98e5da8
Erik Marsh <erik.i.marsh@gmail.com>
2024-02-22 18:08:38 -0800

    Fixed issue where cursor timestamps were not unix timestamps

--- Programs/UserStudy/CursorLogger.cpp
+++ Programs/UserStudy/CursorLogger.cpp
@@ -2,15 +2,36 @@
 
 #include <Input/SimulatedMouse.hpp>
 #include <chrono>
+#include <cstdint>
+#include <ctime>
 #include <condition_variable>
 #include <iostream>
 #include <mutex>
 #include <thread>
+#include <Windows.h>
 
 #include "Logging.hpp"
 
 namespace Logging
 {
+
+uint64_t GetCurrentUnixTimeMillis()
+{
+    // Number of milliseconds between the "Windows epoch"
+    // (Jan 1, 1601 00:00) and the Unix epoch (Jan 1, 1970 00:00).
+    static constexpr uint64_t windowsEpochToUnixEpochMillis = 11644473600000;
+
+    FILETIME filetime;
+    GetSystemTimeAsFileTime(&filetime);
+
+    ULARGE_INTEGER qwFiletime;
+    qwFiletime.LowPart = filetime.dwLowDateTime;
+    qwFiletime.HighPart = filetime.dwHighDateTime;
+
+    const uint64_t timeSince1601_100ns = static_cast<uint64_t>(qwFiletime.QuadPart);
+    const uint64_t timeSince1601_ms = timeSince1601_100ns / 10000;
+    return timeSince1601_ms - windowsEpochToUnixEpochMillis;
+}
 
 void CursorLoggerLoop(SyncState& syncState)
 {
@@ -30,11 +51,11 @@ void CursorLoggerLoop(SyncState& syncState)
             ;
 
         Time loopStart = Clock::now();
-        auto timestamp = std::chrono::time_point_cast<Millis>(loopStart);
+        uint64_t timestamp = GetCurrentUnixTimeMillis();
 
         auto posResult = Input::Mouse::QueryMousePosition();
         Logging::Events::CursorPosition pos{};
-        pos.timestampMillis = timestamp.time_since_epoch().count();
+        pos.timestampMillis = timestamp;
         pos.positionX = posResult.first;
         pos.positionY = posResult.second;
         syncState.logger.Log(pos);
